b968e16f4d1aac0a1fdfff3a11c26e41a7937478
Alexandre <adelaunay@teclib.com>
2012-11-07 08:18:34 +0100

    1st commit (structure) of the new version of mobile (glpi 0.84)

---
+++ hook.php
@@ -0,0 +1,15 @@
+<?php
+
+function plugin_mobile_install() {
+   global $DB;
+
+   return true;
+}
+
+function plugin_mobile_uninstall() {
+   global $DB;
+   
+   return true;
+}
+
+?>
---
+++ setup.php
@@ -0,0 +1,62 @@
+<?php
+
+// Init the hooks of the plugins -Needed
+function plugin_init_mobile() {
+   global $PLUGIN_HOOKS, $LANG;
+   
+   
+   $menu_entry  = 'front/central.php';
+   $PLUGIN_HOOKS['menu_entry']['mobile'] = $menu_entry;
+
+
+   
+   $PLUGIN_HOOKS['redirect_page']['mobile'] = 'front/central.php';
+
+   $plug = new Plugin;
+   if ($plug->isInstalled('mobile') && $plug->isActivated('mobile')) {
+      require_once GLPI_ROOT."/plugins/mobile/inc/common.function.php";
+      checkParams();
+      if (isNavigatorMobile()) redirectMobile();
+   }
+
+}
+
+
+// Get the name and the version of the plugin - Needed
+function plugin_version_mobile() {
+
+   return array('name'           => "Mobile",
+                'version'        => "2.0",
+                'author'         => "<a href='mailto:adelaunay@teclib.com'>Alexandre DELAUNAY</a>",
+                'homepage'       => "http://www.teclib.com/",
+                'minGlpiVersion' => "0.84");
+}
+
+
+// Optional : check prerequisites before install : may print errors or add to message after redirect
+function plugin_mobile_check_prerequisites() {
+
+   if (version_compare(GLPI_VERSION,'0.84','lt')) {
+      return true;
+   } else {
+      echo "GLPI version not compatible need 0.84 min";
+   }
+}
+
+
+// Check configuration process for plugin : need to return true if succeeded
+// Can display a message only if failure and $verbose is true
+function plugin_mobile_check_config($verbose=false) {
+   global $LANG;
+
+   if (true) { // Your configuration check
+      return true;
+   }
+   if ($verbose) {
+      echo $LANG['plugins'][2];
+   }
+   return false;
+}
+
+
+?>
